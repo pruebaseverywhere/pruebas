c80506d7de746c547270b43dbded9ce879d29838
vicbilb <v1c70r69@gmail.com>
2019-02-03 23:38:02 +0100

    Añadida una frase

--- prueba_gits/src/Principal/Principal.java
+++ prueba_gits/src/Principal/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Es preferible reir que llorar");
+		System.out.println("Caguen dos");
 	}
 
 }
